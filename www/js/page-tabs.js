1799acea9da4de0193f826688aae06a001e78741
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2018-07-31 18:23:39 +0100

    added html/css/scss files for in-app tabs layout

---
+++ www/js/page-tabs.js
@@ -0,0 +1,20 @@
+var reel = document.querySelector('.tab_reel');
+var tab1 = document.querySelector('.tab1');
+var tab2 = document.querySelector('.tab2');
+var panel1 = document.querySelector('.tab_panel1');
+var panel2 = document.querySelector('.tab_panel2');
+
+function slideLeft(e) {
+	tab2.classList.remove('active');
+	this.classList.add('active');
+	reel.style.transform = "translateX(0%)";
+}
+
+function slideRight(e) {
+	tab1.classList.remove('active');
+	this.classList.add('active');
+	reel.style.transform = "translateX(-50%)";
+}
+
+tab1.addEventListener('click', slideLeft);
+tab2.addEventListener('click', slideRight);
