a076e9c57e5b09d2ffc2443487c50d8f35f7f1ff
saharma <saharmohdali@gmail.com>
2018-09-28 17:42:55 +0400

    New log in and Registration page

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,41 @@
-(function(ready, EventEmitter) {
+	var data = { 
+		UserPoolId : _config.cognito.userPoolId,
+        ClientId : _config.cognito.clientId
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+    var cognitoUser = userPool.getCurrentUser();
+	
+	window.onload = function(){
+    if (cognitoUser != null) {
 
-  ready(function() {
-    EventEmitter.emit('LoginForm:mount')
-  })
 
-})(window.ready, window.EventEmitter)
+
+        cognitoUser.getSession(function(err, session) {
+
+            if (err) {
+                alert(err);
+                return;
+            }
+            console.log('session validity: ' + session.isValid());
+			
+			
+			cognitoUser.getUserAttributes(function(err, result) {
+				document.getElementById("email_value").innerHTML = result[2].getValue();		
+				if (err) {
+					console.log(err);
+					return;
+				}
+				console.log(result);
+			});			
+			
+        });
+    }
+
+    else     window.location.replace("index.html");
+}
+	function signOut(){
+	    if (cognitoUser != null) {
+          cognitoUser.signOut();
+          window.location.replace("index.html");
+        }
+	}
