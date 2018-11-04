57f623f6d45a003986292ec68479607d7629571c
saharma <saharmohdali@gmail.com>
2018-11-04 14:05:45 +0400

    Updated config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,23 +1,18 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-1_qDLwjDreb',
-        region: 'eu-west-1',
+        region: 'eu-west-1', 
         clientId: '1h00sjqgca9gopqe8pl1es2289'
     },
-};
-export default {
-    s3: {
-        REGION: "eu-west-1",
-        BUCKET: "iscciot"
+    api: {
+        invokeUrl: 'https://3ty7usbhl2.execute-api.eu-west-1.amazonaws.com'
     },
     apiGateway: {
-        REGION: "eu-west-1",
-        URL: "https://3ty7usbhl2.execute-api.eu-west-1.amazonaws.com/prod/"
+        region: "eu-west-1",
+        url: "https://3ty7usbhl2.execute-api.eu-west-1.amazonaws.com",
     },
-    cognito: {
-        REGION: "eu-west-1",
-        USER_POOL_ID: "eu-west-1_qDLwjDreb",
-        APP_CLIENT_ID: "1h00sjqgca9gopqe8pl1es2289",
-        IDENTITY_POOL_ID: "eu-west-1:07ffbfee-410e-444b-9e15-37b4388c2b37"
+    s3: {
+        region: "eu-west-1",
+        bucket: "iscciot"
     }
 };
