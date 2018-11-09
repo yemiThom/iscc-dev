f7436a78f7cb02854a9ff3c141a17b22914917db
saharma <saharmohdali@gmail.com>
2018-11-09 19:25:42 +0400

    Changed configurations

--- www/js/config.js
+++ www/js/config.js
@@ -2,10 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-1_qDLwjDreb',
         region: 'eu-west-1', 
-        clientId: '1h00sjqgca9gopqe8pl1es2289'
-    },
-    api: {
-        invokeUrl: 'https://3ty7usbhl2.execute-api.eu-west-1.amazonaws.com'
+        clientId: '1oagbgbiue3lpcor1l76kluiu'
     },
     apiGateway: {
         region: "eu-west-1",
