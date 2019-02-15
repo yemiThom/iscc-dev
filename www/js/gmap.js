78b946a77b6eb027589fd185cac52190ef26048c
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2019-02-15 15:43:14 +0000

    added request for password reset functionality

--- www/js/gmap.js
+++ www/js/gmap.js
@@ -3,16 +3,16 @@
 function initMap() {
     //element to dump it in (id Map) and options
     var username = localStorage.getItem("username");
-	//start of content href
-	var contStart = "<a href = 'https://google.com/maps/place/";
-	//end of content href
-	var contEnd = "'>Get Directions</a>";
+    //start of content href
+    var contStart = "<a href = 'https://google.com/maps/place/";
+    //end of content href
+    var contEnd = "'>Get Directions</a>";
     //map options
     var options = {
         zoom: 16,
 
         //styling the map.
-        styles: [{ "featureType": "administrative", "elementType": "all", "stylers": [{ "visibility": "on" }, { "lightness": 33 }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2e5d4" }] }, {"featureType": "poi", "elementType": "labels", "stylers":[{ "visibility": "off" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#c5dac6" }] }, { "featureType": "poi.park", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "lightness": 20 }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#c5c6c6" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#e4d7c6" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#fbfaf7" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "visibility": "on" }, { "color": "#acbcc9" }] }]
+        styles: [{ "featureType": "administrative", "elementType": "all", "stylers": [{ "visibility": "on" }, { "lightness": 33 }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2e5d4" }] }, { "featureType": "poi", "elementType": "labels", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#c5dac6" }] }, { "featureType": "poi.park", "elementType": "labels", "stylers": [{ "visibility": "on" }, { "lightness": 20 }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "color": "#c5c6c6" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#e4d7c6" }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#fbfaf7" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "visibility": "on" }, { "color": "#acbcc9" }] }]
 
         //  center:{lat:42.3601, lng:-71.0589}
     }
@@ -24,31 +24,31 @@ function initMap() {
 
 
     //Get current location
-     if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function (position) {
-             initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-             map.setCenter(initialLocation);
-             infoWindow.setPosition(initialLocation);
-             infoWindow.setContent('You are here');
-             infoWindow.open(map);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+            infoWindow.setPosition(initialLocation);
+            infoWindow.setContent('You are here');
+            infoWindow.open(map);
 
-             document.getElementById("placeLat").value = position.coords.latitude;
-             document.getElementById("placeLng").value = position.coords.longitude;
-         });
-     }
-
-
-   $("#bathroomData").click(function(){
-    var title = document.getElementById("placeName").value;
-    var rating = document.getElementById("rating").value;
-    var btype = document.getElementById("btype").value;
-    var lat = document.getElementById("placeLat").value;
-    var lng = document.getElementById("placeLng").value;
-    var created_date = new Date();
+            document.getElementById("placeLat").value = position.coords.latitude;
+            document.getElementById("placeLng").value = position.coords.longitude;
+        });
+    }
 
 
+    $("#bathroomData").click(function () {
+        var title = document.getElementById("placeName").value;
+        var rating = document.getElementById("rating").value;
+        var btype = document.getElementById("btype").value;
+        var lat = document.getElementById("placeLat").value;
+        var lng = document.getElementById("placeLng").value;
+        var created_date = new Date();
 
-    var bathroom = {       
+
+
+        var bathroom = {
             "lat": lat,
             "lng": lng,
             "title": title,
@@ -60,12 +60,12 @@ function initMap() {
             "created_date": created_date
         };
 
-    alert(JSON.stringify(bathroom));
+        alert(JSON.stringify(bathroom));
 
-    console.log(JSON.stringify(bathroom));
-    
+        console.log(JSON.stringify(bathroom));
 
-     //SUBMIT BATHROOM
+
+        //SUBMIT BATHROOM
         $.ajax("https://fast-garden-93601.herokuapp.com/api/bathrooms", {
             data: JSON.stringify(bathroom),
             accept: "application/json",
@@ -79,28 +79,28 @@ function initMap() {
                 alert("Not added");
             }
         });
-});
-   getMap();
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     alert("Geolocation is not supported by this browser.");
-//   }
+    });
+    getMap();
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(showPosition);
+    //   } else {
+    //     alert("Geolocation is not supported by this browser.");
+    //   }
 
 
 
-// function showPosition(position) {
-//   lat = position.coords.latitude;
-//   lng = position.coords.longitude;
-//   map.setCenter(new google.maps.LatLng(lat, lng));
-//   //infoWindow.setPosition(new google.maps.LatLng(lat, lng));
-// infoWindow.setContent('You are here');
-//              infoWindow.open(map);
-//              document.getElementById("placeLat").value = lat;
-//              document.getElementById("placeLng").value = lng;
-// }
+    // function showPosition(position) {
+    //   lat = position.coords.latitude;
+    //   lng = position.coords.longitude;
+    //   map.setCenter(new google.maps.LatLng(lat, lng));
+    //   //infoWindow.setPosition(new google.maps.LatLng(lat, lng));
+    // infoWindow.setContent('You are here');
+    //              infoWindow.open(map);
+    //              document.getElementById("placeLat").value = lat;
+    //              document.getElementById("placeLng").value = lng;
+    // }
 
-//Corresponding to html for Add bathroom 
+    //Corresponding to html for Add bathroom 
 
     var lat = document.getElementById("placeLat").value;
     var lng = document.getElementById("placeLng").value;
@@ -156,7 +156,7 @@ function initMap() {
 
     // ];
 
-    
+
 
     //var review
 
@@ -174,29 +174,29 @@ function initMap() {
     //     }
     // ];
 
-   
+
 
     // addMarker({coords:{lat:42.3601, lng:-71.0589},iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
     //  content:'<h1>You are here </h1>'
     // });
     // addMarker({coords:{lat:42.8584, lng:-70.9300},content:'<h1>Location 1 </h1>'});
     // addMarker({coords:{lat:42.7762, lng:-71.0773},content:'<h1>Location 1 </h1>'});
-    
+
 
 
     // Add Marker Function
     function addMarker(props) {
         var markerIcon;
         //Check for custom icon
-        console.log("props.btype: "+props.btype);
+        console.log("props.btype: " + props.btype);
         if (props.btype == "public") {
             //set Icon Image to public
             //marker.setIcon(props.iconImage);
             markerIcon = "https://s3-eu-west-1.amazonaws.com/iscc-imgs/icons/restroom1.png";
-        }else if(props.btype == "business"){
+        } else if (props.btype == "business") {
             //set Icon Image to business
             markerIcon = "https://s3-eu-west-1.amazonaws.com/iscc-imgs/icons/restroom2.png";
-        }else{
+        } else {
             //set Icon Image to private
             markerIcon = "https://s3-eu-west-1.amazonaws.com/iscc-imgs/icons/restroom3.png";
         }
@@ -214,7 +214,7 @@ function initMap() {
         //Check for content
         if (props.content) {
             var infoWindow = new google.maps.InfoWindow({
-                content: "Title: "  + props.title + "<br><br> Establishment: " + props.btype + "<br><br> Review: " + props.review + "<br><br> Rating: " + props.rating + "<br><br> " + props.content
+                content: "Title: " + props.title + "<br><br> Establishment: " + props.btype + "<br><br> Review: " + props.review + "<br><br> Rating: " + props.rating + "<br><br> " + props.content
             });
 
             marker.addListener('click', function () {
@@ -234,17 +234,17 @@ function initMap() {
             success: function (data) {
                 $.each(data, function (index, element) {
                     console.log(element);
-                     markers.push(JSON.stringify(element));
-                     console.log("markers:" + markers);
-                     addMarker(element);
+                    markers.push(JSON.stringify(element));
+                    console.log("markers:" + markers);
+                    addMarker(element);
                 });
             }
         });
-    //Loop through markers
-    for (var i = 0; i < markers.length; i++) {
-        //Add marker
-        addMarker(markers[i]);
-    }
+        //Loop through markers
+        for (var i = 0; i < markers.length; i++) {
+            //Add marker
+            addMarker(markers[i]);
+        }
     }
 
 
@@ -283,7 +283,7 @@ function initMap() {
     });
 
 
-   
+
 
 
     //SUBMIT REVIEW
