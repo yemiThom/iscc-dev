f412e1b1d2a23397d85b2a02862436ef190c60d9
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2019-03-06 11:40:31 +0000

    changes for ios styling 4

--- www/js/index.js
+++ www/js/index.js
@@ -8,8 +8,40 @@ var maintenancemsg = document.getElementById("maintenancemsg");
 
 //window.onload = function () {
 $(document).ready(function () {
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    /* if the device is not ios hide the download button */
+    if(isMobile.iOS()){
+           alert('this is an ios device');  
+           /*var headerElem = document.getElementsByClassName('default-header');
+           for(var i=0; i < headerElem.length; i++){
+               headerElem[i].classList.add("ios-top-padding");
+           }*/
 
-     $.ajax("https://fast-garden-93601.herokuapp.com/api/maintenancemessages", {
+           $(".default-header").each(function(){
+                $(this).addClass("ios-top-padding");
+           });
+    };
+
+    $.ajax("https://fast-garden-93601.herokuapp.com/api/maintenancemessages", {
         data: { get_param: 'value' },
         type: 'GET',
         dataType: 'json',
@@ -17,13 +49,9 @@ $(document).ready(function () {
             $.each(data, function (index, element) {
                 $('#maintenancemsg').html(element.msg);
             });
-    
-}
+
+        }
     });
-
-
-
-
 
     if (cognitoUser != null) {
         cognitoUser.getSession(function (err, session) {
@@ -39,7 +67,7 @@ $(document).ready(function () {
                 document.getElementById("email_value").innerHTML = result[2].getValue();
                 //checkEmailValContent(result[2].getValue());
                 localStorage.setItem("username", result[2].getValue());
-                console.log("username[localstorage]: "+localStorage.getItem("username"));
+                console.log("username[localstorage]: " + localStorage.getItem("username"));
                 if (err) {
                     console.log(err);
                     return;
