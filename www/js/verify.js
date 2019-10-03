0993dc58a2aa4670d733906e9d4ec7d3e5d5b063
Sahar Mohamed-Ali <saharmohdali@gmail.com>
2019-10-03 12:11:45 +0400

    Update verify.js

--- www/js/verify.js
+++ www/js/verify.js
@@ -1,29 +1,44 @@
 
-
+var emailInput;
+var passwordInput;
 
 
 function loginOnApi(){
+
+
+  alert(localStorage.getItem("email"));
+
+  if (localStorage.getItem("email") === null || localStorage.getItem("email") === ''){
+    alert("email is null tho");
+    localStorage.setItem("email", $('#inputUsername').val());
+    emailInput = localStorage.getItem("email");
+    alert("email set " + localStorage.getItem("email") );
+  };
+
+  if (localStorage.getItem("password") === null || localStorage.getItem("password") === ''){
+    localStorage.setItem("password", $('#inputPassword').val());
+    passwordInput = localStorage.getItem("password");
+    alert("password set " + localStorage.getItem("password") );
+  }
+
   var data = {
-    username : $('#inputUsername').val(),
-    password : $('#inputPassword').val()
+    email: localStorage.getItem("email"),
+    password: localStorage.getItem("password")
   }
 
   var memberData = {
     accessToken : '',
     accountId : ''
-  
+
   }
 
   var invoiceData = {
     accessToken : '',
     accountId : ''
-  
+
   }
 
-
-
-
-  if(data.username == ""){
+  if(data.email == ""){
     alert('Please enter user email.');
     return;203.
 
@@ -33,24 +48,26 @@ function loginOnApi(){
     return;
   }
 
-  var loginData = 'username='+data.username+'&password='+data.password;
+  var loginData = 'username='+data.email+'&password='+data.password;
+  alert("loginData: " + loginData);
 
+//login data
 var settings = {
   "async": true,
   "crossDomain": true,
   "url": "https://infinite-thicket-67578.herokuapp.com/login",
   //  "url": "http://localhost:3000/login",
-  
+
   "method": "POST",
   "headers": {
     "content-type": "application/x-www-form-urlencoded"
 
   },
   "processData": false,
-  "data": loginData 
+  "data": loginData
 }
 
-
+//member settings
 var memberSettings = {
   "async": true,
   "crossDomain": true,
@@ -62,9 +79,10 @@ var memberSettings = {
 
   },
   "processData": false,
-  "data": memberData 
+  "data": memberData
 }
 
+//invoices settings
 var invoiceSettings = {
   "async": true,
   "crossDomain": true,
@@ -76,7 +94,7 @@ var invoiceSettings = {
 
   },
   "processData": false,
-  "data": invoiceData 
+  "data": invoiceData
 }
 
 
@@ -91,7 +109,7 @@ $.ajax(settings).done(function (response) {
     memberData.accountId = response.Permissions[0].AccountId;
     getInvoiceData();
     getMemberData();
-  
+
   }
   else{
       console.log(response.error_description);
@@ -99,27 +117,29 @@ $.ajax(settings).done(function (response) {
 
 });
 
+//Function for Invoice Data
 function getInvoiceData(){
 
    let invoiceDataSend = `accessToken=${invoiceData.accessToken}&accountId=${invoiceData.accountId}`;
   invoiceSettings.data = invoiceDataSend;
-  
+
   $.ajax(memberSettings).done(function (response) {
 
     if(response[0].Name)
     {
       localStorage.setItem("Invoices", response[0].Name);
       ;
-  
-      
+
+
     }
     else{
         alert('No invoices');
     }
-  
+
   });
 }
- 
+
+//Function for Member Data
 function getMemberData(){
 
   let memberDataSend = `accessToken=${memberData.accessToken}&accountId=${memberData.accountId}`;
@@ -131,29 +151,29 @@ function getMemberData(){
     {
       localStorage.setItem("MemberType", response[0].Name);
       window.location.href = "home.html";
-  
-      
+
+
     }
     else{
         alert('Not registered with member type.');
     }
-  
+
   });
 }
 
 
-    
+
 
 }
 
 var mType = localStorage.getItem("MemberType");
 
 
- 
+
 
 function disconnectISCCaccount(){
 
   localStorage.removeItem("access_token");
   window.location.href = "home.html";
-  
+
 }
